e522dc79a06a77b5e69d0414454420c995b3e80c
fr <1959370287@qq.com>
2025-09-12 17:10:24 +0800

    dummy

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -17,13 +17,13 @@
 #include <cpu/cpu.h>
 #include <cpu/ifetch.h>
 #include <cpu/decode.h>
-
+#include "memory/paddr.h"
 #define R(i) gpr(i)
 #define Mr vaddr_read
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S,
+  TYPE_I, TYPE_U, TYPE_S,TYPE_J,
   TYPE_N, // none
 };
 
@@ -32,7 +32,7 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-
+#define immJ() do { *imm = SEXT(((BITS(i, 31, 31) << 20) | (BITS(i, 19, 12) << 12) | (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1)), 21); } while(0)
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst;
   int rs1 = BITS(i, 19, 15);
@@ -43,6 +43,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_N: break;
+    case TYPE_J:                   immJ(); break;
     default: panic("unsupported type = %d", type);
   }
 }
@@ -62,9 +63,38 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
   INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu    , I, R(rd) = Mr(src1 + imm, 1));
   INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb     , S, Mw(src1 + imm, 1, src2));
+  INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(rd) = src1 + imm);
+  
+  // jal (Jump and Link) - J-Type
+  INSTPAT("????? ????? ????? ??? ????? 1101111", jal, J, {
+  // 1. Link: 如果目标寄存器 rd 不是 x0, 就保存返回地址 (pc + 4)
+  if (rd != 0) {
+    cpu.gpr[rd] = s->pc + 4;
+  }
+  // 2. Jump: 设置下一条指令的PC为当前PC + 立即数
+  s->dnpc = s->pc + imm;
+  });
 
+INSTPAT("????? ????? ????? 000 ????? 1100111", jalr, I, {
+  // 暂存一下pc + 4, 作为可能的返回地址
+  word_t link_addr = s->pc + 4;
+
+  // 直接使用 src1 (代表rs1寄存器的值) 和 imm (代表立即数的值)
+  // 计算跳转目标地址，并确保最低位为0
+  s->dnpc = (src1 + imm) & ~1;
+
+  // 如果rd不是x0, 才需要保存返回地址
+  if (rd != 0) {
+    // 这里我们假设可以直接用 rd (寄存器编号) 和 cpu.gpr 来写入
+    // 这与您之前成功的修复风格一致
+    cpu.gpr[rd] = link_addr;
+  }
+});
+  // sw (Store Word) - S-Type
+  INSTPAT("??????? ????? ????? 010 ????? 0100011", sw, S, Mw(src1 + imm, 4, src2));
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
+
   INSTPAT_END();
 
   R(0) = 0; // reset $zero to 0
